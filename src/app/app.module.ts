75f631bae804aa59e20909c0de6fd95b1ed6fac3
Neil <neilbo21@gmail.com>
2019-09-21 12:17:52 +1000

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CONFIG } from './firebaseConfig';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBpQtd-a4ucZQHmaajuJtHIjxLaQtMWlsE",
+    authDomain: "whatsupkid-be300.firebaseapp.com",
+    databaseURL: "https://whatsupkid-be300.firebaseio.com",
+    projectId: "whatsupkid-be300",
+    storageBucket: "whatsupkid-be300.appspot.com",
+    messagingSenderId: "1026075383861",
+    appId: "1:1026075383861:web:4d90ba4fd9722c5869b97c"
+  };
